b9bd41a1721d9fa4573bd52c96500cb567e22d68
Benoît Hubert <benoithubert@gmail.com>
2021-01-10 09:44:59 +0100

    rename API endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { port } = require('./config');
 const db = require('./mongo-client');
+const { mongo: { database } } = require('./config');
 require('./run');
 
 const app = express();
@@ -9,13 +10,14 @@ app.get('/', (req, res) => {
   res.send({ status: 'ok' });
 });
 
-app.get('/movies', (req, res) => {
-  db.db('moviesdb').collection('movies').find({}).toArray((err, movies) => {
+app.get('/api/questions-stats', (req, res) => {
+  const dbo = db.db(database);
+  dbo.collection('randomQuestions').find({}).toArray((err, stats) => {
     if (err) {
       console.error(err);
       return res.sendStatus(500);
     }
-    res.send(movies);
+    res.send(stats);
   });
 });
 
